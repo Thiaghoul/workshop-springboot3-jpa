b3b87e1ee0fc5449af2f2f99a6eed3e2d81692ce
thiaghoul <thiagohnunes.w@gmail.com>
2024-11-11 23:22:22 -0300

    Service layer, component registration

--- src/main/java/com/darkra/course/repositories/UserRepository.java
+++ src/main/java/com/darkra/course/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.darkra.course.repositories;
 
 import com.darkra.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
