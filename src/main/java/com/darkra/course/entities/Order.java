0a4fc77f9019c9438455448c0c7f1f9c4ce788a2
thiaghoul <thiagohnunes.w@gmail.com>
2024-11-15 15:21:02 -0300

    ENUM ORDER STATUS

--- src/main/java/com/darkra/course/entities/Order.java
+++ src/main/java/com/darkra/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.darkra.course.entities;
 
+import com.darkra.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -25,13 +26,16 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
 
     }
 
-    public Order(Long Id, Instant moment, User client) {
+    public Order(Long Id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -57,6 +61,16 @@ public class Order implements Serializable {
 
     public void setUser(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(this.orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
