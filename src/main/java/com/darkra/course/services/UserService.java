2cb78e3fb3c8bf32112cc1b8f69c3770eb50c71b
thiaghoul <thiagohnunes.w@gmail.com>
2024-11-17 12:55:36 -0300

    User insert

--- src/main/java/com/darkra/course/services/UserService.java
+++ src/main/java/com/darkra/course/services/UserService.java
@@ -18,6 +18,10 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User insert(User obj){
+        return userRepository.save(obj);
+    }
+
     public User findById(Long id){
        Optional<User> obj = userRepository.findById(id);
        return obj.get();
