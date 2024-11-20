27932ff4b3985b4a15ac7d417803cec0953b4954
thiaghoul <thiagohnunes.w@gmail.com>
2024-11-20 14:59:51 -0300

    http methods delete/post/put - Category

--- src/main/java/com/darkra/course/services/CategoryService.java
+++ src/main/java/com/darkra/course/services/CategoryService.java
@@ -4,7 +4,11 @@ import com.darkra.course.entities.Category;
 import com.darkra.course.entities.Order;
 import com.darkra.course.repositories.CategoryRepository;
 import com.darkra.course.repositories.OrderRepository;
+import com.darkra.course.services.exceptions.DatabaseException;
+import com.darkra.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,8 +24,42 @@ public class CategoryService {
         return categoryRepository.findAll();
     }
 
+    public Category insert(Category obj){
+        return categoryRepository.save(obj);
+    }
+
     public Category findById(Long id){
        Optional<Category> obj = categoryRepository.findById(id);
-       return obj.get();
+       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public void delete(Long id){
+        if(!categoryRepository.existsById(id)){
+            throw new ResourceNotFoundException(id);
+        }
+        try{
+            categoryRepository.deleteById(id);
+
+        }catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Category update(Long id, Category obj){
+        try{
+            Category entity = categoryRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return categoryRepository.save(entity);
+
+        }catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public void updateData(Category entity, Category obj){
+        entity.setName(obj.getName());
+    }
+
+
+
 }
